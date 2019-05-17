c84a5ba000dcb7408550779fafc83d103d07a6fc
Ryan Carr <ryancarr@users.noreply.github.com>
2019-05-17 13:27:30 -0500

    Initial commit of GradescopeExportRenamer
    Contains Pseudocode for project

---
+++ GradescpeExportRenamer.py
@@ -0,0 +1,15 @@
+# Name     : Gradescope Export Renamer
+# Author   : Ryan Carr
+# Modified : 05/17/2019
+# Purppose : Opens .yml file in exports folder and renames .pdfs according
+# to student name.
+
+# Ask user for full path to .yml file
+
+# Open yml file and skip first line
+
+# Process file one submission at a time storing filename and student name in
+# a dictionary
+
+# Prompt user for permission before rewriting files
+# iterate through dictionary renaming files
